81dbcf4e46d34610f92bc46942a1e4d8cb473fc7
David Han <davidhan.5.25@gmail.com>
2019-04-24 22:21:21 -0700

    Add education and experience to profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -8,8 +8,71 @@ const requireLogin = require('../../middlewares/requireLogin');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
+// @route   GET api/profiles/all
+// @desc    Get all profiles
+// @access  Public
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate('user', ['name', 'email'])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofiles = 'There are no profiles';
+        return res.status(404).json(errors);
+      }
+      return res.json(profiles);
+    })
+    .catch(err => {
+      errors.noprofiles = 'There are no profiles';
+      return res.status(404).json(errors);
+    });
+});
+
+// @route   GET api/profiles/handle/:handle
+// @desc    Get a profile by handle
+// @access  Public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'email'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'No profile found';
+        return res.status(404).json(errors);
+      }
+      return res.json(profile);
+    })
+    .catch(err => {
+      errors.noprofile = 'No profile found';
+      res.status(404).json(errors);
+    });
+});
+
+// @route   GET api/profiles/user/:user_id
+// @desc    Get a profile by user ID
+// @access  Public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'email'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'No profile found';
+        return res.status(404).json(errors);
+      }
+      return res.json(profile);
+    })
+    .catch(err => {
+      errors.noprofile = 'No profile found';
+      res.status(404).json(errors);
+    });
+});
+
 // @route   GET api/profiles/
-// @desc    Get current users profile
+// @desc    Get logged in users profile
 // @access  Private
 router.get('/', requireLogin, async (req, res) => {
   const errors = {};
@@ -27,7 +90,7 @@ router.get('/', requireLogin, async (req, res) => {
 });
 
 // @route   POST api/profiles/
-// @desc    Create & Edit users profile
+// @desc    Create / Edit users profile
 // @access  Private
 router.post('/', (req, res) => {
   const errors = {};
@@ -44,7 +107,7 @@ router.post('/', (req, res) => {
 
   const profileFields = {
     ...req.body,
-    user: req.body.id,
+    user: req.body.id, // REPLACE
     interests: interests.split(',').map(x => x.trim()),
     social: {}
   };
@@ -59,11 +122,11 @@ router.post('/', (req, res) => {
         https: true
       });
   });
-
+  // REPLACE
   Profile.findOne({ user: req.body.id }).then(profile => {
     if (profile) {
       Profile.findOneAndUpdate(
-        { user: req.body.id },
+        { user: req.body.id }, // REPLACE
         { $set: profileFields },
         { new: true }
       ).then(profile => res.json(profile));
@@ -77,6 +140,38 @@ router.post('/', (req, res) => {
       });
     }
   });
+});
+
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post('/experience', (req, res) => {
+  Profile.findOne({ user: req.body.id }) //REPLACE
+    .then(profile => {
+      const newExp = { ...req.body };
+
+      // Add to exp array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   POST api/profile/education
+// @desc    Add education to profile
+// @access  Private
+router.post('/education', (req, res) => {
+  Profile.findOne({ user: req.body.id }) //REPLACE
+    .then(profile => {
+      const newEdu = { ...req.body };
+
+      // Add to exp array
+      profile.education.unshift(newEdu);
+
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
 });
 
 module.exports = router;
