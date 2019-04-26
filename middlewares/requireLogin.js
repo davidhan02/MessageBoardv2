01e230cc483508d6daf1411bb1f2da8088c4998e
David Han <davidhan.5.25@gmail.com>
2019-04-26 16:58:31 -0700

    Going to try something

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
-  if (req.user) {
-    return next();
+  if (!req.user) {
+    return res.status(401).json({ error: 'You must log in!' });
   }
-  res.status(401).json({ error: 'You must log in!' });
+  return next();
 };
