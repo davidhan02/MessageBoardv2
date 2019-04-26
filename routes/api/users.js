41b25507999a44e851ef32605d69d1f53ed5ab9e
David Han <davidhan.5.25@gmail.com>
2019-04-26 12:28:31 -0700

    Refactor users routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -27,16 +27,10 @@ router.post('/login', (req, res, next) => {
   const errors = {};
 
   passport.authenticate('local', (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
     if (!user) {
       return res.status(400).json(info);
     }
     req.login(user, err => {
-      if (err) {
-        return next(err);
-      }
       return res.json(user);
     });
   })(req, res, next);
@@ -45,26 +39,17 @@ router.post('/login', (req, res, next) => {
 // @route   POST api/users/register
 // @desc    Register a user
 // @access  Public
-router.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
+router.post('/register', async (req, res) => {
   const errors = {};
-
-  User.findOne({ email }).then(user => {
-    if (user) {
-      errors.email = 'Email already exists';
-      return res.status(400).json(errors);
-    } else {
-      const newUser = new User({
-        name,
-        email,
-        password
-      });
-      newUser
-        .save()
-        .then(user => res.json(user))
-        .catch(err => console.log(err));
-    }
-  });
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    errors.email = 'Email already exists';
+    return res.status(400).json(errors);
+  } else {
+    const newUser = await new User({ ...req.body }).save();
+    return res.json(newUser);
+  }
 });
 
 module.exports = router;
