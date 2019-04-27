ecda5f61a250475d6729f18927487a39deb881d9
David Han <davidhan.5.25@gmail.com>
2019-04-26 23:40:23 -0700

    Basic create-profile started

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,19 +2,44 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import PropTypes from 'prop-types';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 import { clearErrors } from '../../actions/authActions';
 import { submitProfile } from '../../actions/profileActions';
 
 class CreateProfile extends Component {
+  onSubmit = formValues => {
+    const { submitProfile, history } = this.props;
+    submitProfile(formValues, history);
+  };
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <div>
-        <h1>CreateProfile</h1>
-      </div>
+      <Row className="create-profile">
+        <Col md={8} className="m-auto">
+          <h1 className="display-4 text-center">Create Profile</h1>
+          <p className="lead text-center">Input some of your information</p>
+          <Form onSubmit={handleSubmit(this.onSubmit)}>
+            <Field name="test" type="text" component="input" />
+            <Button variant="primary" size="lg" type="submit" block>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
     );
   }
 }
+
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  submitProfile: PropTypes.func.isRequired,
+  clearErrors: PropTypes.func.isRequired
+};
 
 const mapStateToProps = ({ profile, errors }) => ({ profile, errors });
 
