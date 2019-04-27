6e3dc954f3bb6fdb2bb4a1b235e5f62e466b731f
David Han <davidhan.5.25@gmail.com>
2019-04-26 21:41:14 -0700

    Getting dashboardContent to render conditionally

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import { getCurrentProfile } from '../../actions/profilesActions';
 
 class Dashboard extends Component {
@@ -8,19 +11,37 @@ class Dashboard extends Component {
     this.props.getCurrentProfile();
   }
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profiles;
+
+    let dashboardContent;
+
+    if (profile === null || loading) {
+      dashboardContent = <h4>Loading...</h4>;
+    } else {
+      dashboardContent = <h1>Hello {user.name}</h1>;
+    }
+
     return (
-      <div>
-        <h1>Dashboard</h1>
-      </div>
+      <Row className="dashboard">
+        <Col md={12}>
+          <h1 className="display-4">Dashboard</h1>
+          {dashboardContent}
+        </Col>
+      </Row>
     );
   }
 }
 
 Dashboard.propTypes = {
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
   getCurrentProfile: PropTypes.func.isRequired
 };
 
+const mapStateToProps = ({ auth, profiles }) => ({ auth, profiles });
+
 export default connect(
-  null,
+  mapStateToProps,
   { getCurrentProfile }
 )(Dashboard);
