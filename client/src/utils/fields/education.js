c96106249b2cc6e2c15cd88b51741176a60c556c
David Han <davidhan.5.25@gmail.com>
2019-04-30 00:27:33 -0700

    Create education fields for future mapping

---
+++ client/src/utils/fields/education.js
@@ -0,0 +1,8 @@
+const eduFields = [
+  { label: 'School', name: 'school', type: 'text' },
+  { label: 'Degree', name: 'degree', type: 'text' },
+  { label: 'Major', name: 'major', type: 'text' },
+  { label: 'From Date', name: 'from', type: 'date' }
+];
+
+export default eduFields;
