9cf7531d312af1e3928645c3ec6f05511aa0d3d5
David Han <davidhan.5.25@gmail.com>
2019-04-30 00:35:58 -0700

    Wired up AddEducation to Router in App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 
 import PrivateRoute from './common/PrivateRoute';
 import AddExperience from './credentials/AddExperience';
+import AddEducation from './credentials/AddEducation';
 import Dashboard from './dashboard/Dashboard';
 import Register from './auth/Register';
 import Navbar from './layout/Navbar';
@@ -27,6 +28,7 @@ const App = () => {
             <Route exact path="/register" component={Register} />
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
             <PrivateRoute exact path="/add-exp" component={AddExperience} />
+            <PrivateRoute exact path="/add-edu" component={AddEducation} />
             <Route component={NotFound} />
           </Switch>
         </Container>
