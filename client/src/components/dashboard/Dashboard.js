1b8143f722f0b1fd6a0970ea92fca5f30ab929ff
David Han <davidhan.5.25@gmail.com>
2019-04-30 15:39:01 -0700

    Fixed conditional rendering on Dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -8,10 +8,38 @@ import PropTypes from 'prop-types';
 import * as profileActions from '../../actions/profileActions';
 import CreateProfile from '../profile/edit/CreateProfile';
 import Spinner from '../common/spinner/Spinner';
+import ProfileButtons from './ProfileButtons';
+import isEmpty from '../../utils/is-empty';
 
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentProfile();
+  }
+
+  renderHeader() {
+    const { profile } = this.props.profiles;
+    if (isEmpty(profile))
+      return (
+        <p className="lead">
+          You don't have a profile, but you can create one here:
+        </p>
+      );
+    return (
+      <div className="m-3">
+        <ProfileButtons />
+      </div>
+    );
+  }
+
+  renderContent() {
+    const { profile, loading } = this.props.profiles;
+    if (profile === null || loading) {
+      return <Spinner />;
+    }
+    if (isEmpty(profile)) {
+      return <CreateProfile />;
+    }
+    return <h1>Dashboard display profile</h1>;
   }
 
   onDeleteClick = e => {
@@ -20,15 +48,15 @@ class Dashboard extends Component {
 
   render() {
     const { user } = this.props.auth;
-    const { profile, loading } = this.props.profiles;
     return (
       <Fragment>
         <Row className="dashboard">
-          <Col className="text-center">
+          <Col lg={12} className="text-center">
             <h1 className="display-4">Welcome {user.name}</h1>
+            {this.renderHeader()}
           </Col>
         </Row>
-        {profile === null || loading ? <Spinner /> : <CreateProfile />}
+        {this.renderContent()}
         <Row>
           <Col className="m-3 text-center">
             <Button variant="danger" onClick={this.onDeleteClick}>
