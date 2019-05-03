33d6409542d37380303c91d9c774a14adac73938
David Han <davidhan.5.25@gmail.com>
2019-05-02 18:09:28 -0700

    Reordered Company field

--- client/src/utils/fields/experience.js
+++ client/src/utils/fields/experience.js
@@ -1,6 +1,6 @@
 const expFields = [
-  { label: 'Position', name: 'title', type: 'text' },
   { label: 'Company', name: 'company', type: 'text' },
+  { label: 'Position', name: 'title', type: 'text' },
   { label: 'Location', name: 'location', type: 'text' },
   { label: 'From Date', name: 'from', type: 'date' }
 ];
