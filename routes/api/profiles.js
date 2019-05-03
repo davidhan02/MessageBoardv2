dd7e5a5ee80f4e4d1f4333cf48541e2abc0752df
David Han <davidhan.5.25@gmail.com>
2019-05-02 18:02:43 -0700

    Changed all of the routes in profiles to async / await format

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -11,87 +11,75 @@ mongoose.set('useFindAndModify', false);
 // @route   GET api/profiles/all
 // @desc    Get all profiles
 // @access  Public
-router.get('/all', (req, res) => {
-  const errors = {};
-
-  Profile.find()
-    .populate('user', ['name', 'email'])
-    .then(profiles => {
-      if (!profiles) {
-        errors.serverErr = 'No profiles found';
-        return res.status(500).json(errors);
-      }
-      res.json(profiles);
-    })
-    .catch(err => {
-      console.error(err.message);
-      errors.serverErr = 'No profiles found';
-      res.status(500).json(errors);
-    });
+router.get('/all', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', 'name');
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'No profiles found' });
+  }
 });
 
 // @route   GET api/profiles/handle/:handle
 // @desc    Get a profile by handle
 // @access  Public
-router.get('/handle/:handle', (req, res) => {
+router.get('/handle/:handle', async (req, res) => {
   const errors = {};
-
-  Profile.findOne({ handle: req.params.handle })
-    .populate('user', ['name', 'email'])
-    .then(profile => {
-      if (!profile) {
-        errors.serverErr = 'No profile found';
-        return res.status(500).json(errors);
-      }
-      res.json(profile);
-    })
-    .catch(err => {
-      console.error(err.message);
-      errors.serverErr = 'No matching handle';
-      res.status(500).json(errors);
-    });
+  try {
+    const profile = await Profile.findOne({
+      handle: req.params.handle
+    }).populate('user', ['name', 'email']);
+    if (!profile) {
+      errors.msg = 'No profile found for this user';
+      return res.status(400).json(errors);
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    errors.msg = 'No user matching handle';
+    res.status(500).json(errors);
+  }
 });
 
 // @route   GET api/profiles/user/:user_id
 // @desc    Get a profile by user ID
 // @access  Public
-router.get('/user/:user_id', (req, res) => {
+router.get('/user/:user_id', async (req, res) => {
   const errors = {};
-
-  Profile.findOne({ user: req.params.user_id })
-    .populate('user', ['name', 'email'])
-    .then(profile => {
-      if (!profile) {
-        errors.serverErr = 'No profile found';
-        return res.status(500).json(errors);
-      }
-      res.json(profile);
-    })
-    .catch(err => {
-      console.error(err.message);
-      errors.serverErr = 'No matching user ID';
-      res.status(500).json(errors);
-    });
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'email']);
+    if (!profile) {
+      errors.msg = 'No profile found for this user';
+      return res.status(400).json(errors);
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    errors.msg = 'No matching user ID';
+    res.status(500).json(errors);
+  }
 });
 
 // @route   GET api/profiles/me
 // @desc    Get logged in users profile
 // @access  Private
-router.get('/me', requireLogin, async ({ user: { id } }, res) => {
+router.get('/me', requireLogin, async (req, res) => {
+  const errors = {};
   try {
-    const profile = await Profile.findOne({ user: id }).populate('user', [
-      'name',
-      'email'
-    ]);
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'email']);
     if (!profile) {
-      const errors = {};
-      errors.serverErr = 'No profile found for this user';
-      return res.status(500).json(errors);
+      errors.msg = 'No profile found for this user';
+      return res.status(400).json(errors);
     }
     res.json(profile);
   } catch (err) {
     console.error(err.message);
-    errors.serverErr = 'Server Error';
+    errors.msg = 'Server Error';
     res.status(500).json(errors);
   }
 });
@@ -133,7 +121,7 @@ router.post('/me', requireLogin, async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.error(err.message);
-    errors.serverErr = 'Server Error';
+    errors.msg = 'Server Error';
     res.status(500).json(errors);
   }
 });
@@ -141,100 +129,118 @@ router.post('/me', requireLogin, async (req, res) => {
 // @route   POST api/profiles/experience
 // @desc    Add experience to profile
 // @access  Private
-router.post('/experience', requireLogin, (req, res) => {
+router.post('/experience', requireLogin, async (req, res) => {
   const errors = {};
-
-  Profile.findOne({ user: req.user.id })
-    .then(profile => {
-      profile.experience.unshift({ ...req.body });
-
-      profile.save().then(profile => res.json(profile));
-    })
-    .catch(err => {
-      console.error(err.message);
-      errors.serverErr = 'No matching user ID';
-      res.status(500).json(errors);
-    });
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    profile.experience.unshift({ ...req.body });
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    errors.msg = 'No matching user ID';
+    res.status(500).json(errors);
+  }
 });
 
 // @route   POST api/profiles/education
 // @desc    Add education to profile
 // @access  Private
-router.post('/education', requireLogin, (req, res) => {
+router.post('/education', requireLogin, async (req, res) => {
   const errors = {};
-
-  Profile.findOne({ user: req.user.id })
-    .then(profile => {
-      profile.education.unshift({ ...req.body });
-
-      profile.save().then(profile => res.json(profile));
-    })
-    .catch(err => {
-      console.error(err.message);
-      errors.serverErr = 'No matching user ID';
-      res.status(500).json(errors);
-    });
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    profile.education.unshift({ ...req.body });
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    errors.msg = 'No matching user ID';
+    res.status(500).json(errors);
+  }
 });
 
 // @route   PUT api/profiles/experience/:exp_id
 // @desc    Replace experience in profile
 // @access  Private
-router.put('/experience/:exp_id', requireLogin, (req, res) => {
-  Profile.findOneAndUpdate(
-    { user: req.user.id, 'experience._id': req.params.exp_id },
-    { $set: { 'experience.$': { ...req.body } } },
-    { new: true }
-  )
-    .then(profile => res.json(profile))
-    .catch(err => res.status(500).json(err));
+router.put('/experience/:exp_id', requireLogin, async (req, res) => {
+  try {
+    const profile = await Profile.findOneAndUpdate(
+      { user: req.user.id, 'experience._id': req.params.exp_id },
+      { $set: { 'experience.$': { ...req.body } } },
+      { new: true }
+    );
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    errors.msg = 'No matching experience ID';
+    res.status(500).json(errors);
+  }
 });
 
 // @route   PUT api/profiles/education/:edu_id
 // @desc    Replace education in profile
 // @access  Private
-router.put('/education/:edu_id', requireLogin, (req, res) => {
-  Profile.findOneAndUpdate(
-    { user: req.user.id, 'education._id': req.params.edu_id },
-    { $set: { 'education.$': { ...req.body } } },
-    { new: true }
-  )
-    .then(profile => res.json(profile))
-    .catch(err => res.status(500).json(err));
+router.put('/education/:edu_id', requireLogin, async (req, res) => {
+  try {
+    const profile = await Profile.findOneAndUpdate(
+      { user: req.user.id, 'education._id': req.params.edu_id },
+      { $set: { 'education.$': { ...req.body } } },
+      { new: true }
+    );
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    errors.msg = 'No matching education ID';
+    res.status(500).json(errors);
+  }
 });
 
 // @route   DELETE api/profiles/experience/:exp_id
 // @desc    Delete experience from profile
 // @access  Private
-router.delete('/experience/:exp_id', requireLogin, (req, res) => {
-  Profile.findOne({ user: req.user.id })
-    .then(profile => {
-      profile.experience.pull(req.params.exp_id);
-
-      profile.save().then(profile => res.json(profile));
-    })
-    .catch(err => res.status(500).json(err));
+router.delete('/experience/:exp_id', requireLogin, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    profile.experience.pull(req.params.exp_id);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    errors.msg = 'No matching experience ID';
+    res.status(500).json(errors);
+  }
 });
 
 // @route   DELETE api/profiles/education/:edu_id
 // @desc    Delete education from profile
 // @access  Private
-router.delete('/education/:edu_id', requireLogin, (req, res) => {
-  Profile.findOne({ user: req.user.id })
-    .then(profile => {
-      profile.education.pull(req.params.edu_id);
-
-      profile.save().then(profile => res.json(profile));
-    })
-    .catch(err => res.status(500).json(err));
+router.delete('/education/:edu_id', requireLogin, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    profile.education.pull(req.params.edu_id);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    errors.msg = 'No matching education ID';
+    res.status(500).json(errors);
+  }
 });
 
 // @route   DELETE api/profiles/me
 // @desc    Delete user and profile
 // @access  Private
 router.delete('/me', requireLogin, async (req, res) => {
-  await Profile.findOneAndRemove({ user: req.user.id });
-  await User.findOneAndRemove({ _id: req.user.id });
-  res.json({ success: true });
+  try {
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ success: true });
+  } catch (err) {
+    console.error(err.message);
+    errors.msg = 'Server Error';
+    res.status(500).json(errors);
+  }
 });
 
 module.exports = router;
