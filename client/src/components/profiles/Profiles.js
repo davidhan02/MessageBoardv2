53f763180ae9c030f7fa6ee3a7ae17409bdf50c1
David Han <davidhan.5.25@gmail.com>
2019-05-02 18:34:12 -0700

    Created Profiles and ProfilesItem

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import ProfileItem from './ProfileItem';
+import Spinner from '../common/spinner/Spinner';
+import getProfiles from '../../actions/profileActions';
+
+class Profiles extends Component {
+  componentDidMount() {
+    this.props.getProfiles();
+  }
+
+  render() {
+    const { profiles, loading } = this.props.profiles;
+    let profileItems;
+
+    if (profiles === null || loading) {
+      profileItems = <Spinner />;
+    } else {
+      if (profiles.length > 0) {
+        profileItems = <h1>Display profiles here...</h1>;
+      } else {
+        profileItems = <h4>No profiles found...</h4>;
+      }
+    }
+    return (
+      <Row>
+        <Col md={12}>
+          <h1 className="display-4 text-center">User Profiles</h1>
+          <p className="lead text-center">
+            Browse and connect with other users
+          </p>
+          {profileItems}
+        </Col>
+      </Row>
+    );
+  }
+}
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profiles: PropTypes.object.isRequired
+};
+
+const mapStateToProps = ({ profiles }) => ({ profiles });
+
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
