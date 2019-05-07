fd8a2344c9b9e4c83452677ae5758da7e1bcafc0
David Han <davidhan.5.25@gmail.com>
2019-05-06 17:28:57 -0700

    Created CommentForm

---
+++ client/src/components/comments/CommentForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { reduxForm, Field } from 'redux-form';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import AreaField from '../common/fields/AreaField';
+import { submitComment } from '../../actions/postActions';
+
+class CommentForm extends Component {
+  onSubmit = comment => this.props.submitComment(comment);
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <Form onSubmit={handleSubmit(this.onSubmit)}>
+        <Field
+          name="text"
+          label="Comment"
+          controlId="comment"
+          component={AreaField}
+        />
+        <Button type="submit">Submit Comment</Button>
+      </Form>
+    );
+  }
+}
+
+const mapDispatchToProps = { submitComment };
+
+const enhance = compose(
+  reduxForm({ form: 'commentForm' }),
+  connect(
+    null,
+    mapDispatchToProps
+  )
+);
+
+export default enhance(CommentForm);
