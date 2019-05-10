ed880e410d53f685a2468dd19d39e04547e922ea
David Han <davidhan.5.25@gmail.com>
2019-05-09 21:26:50 -0700

    Trying to make routing work on deployment

--- server.js
+++ server.js
@@ -35,16 +35,24 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+
+  app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+
+  app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use('/auth/google', google);
 app.use('/api/profiles', profiles);
 app.use('/api/users', users);
 app.use('/api/posts', posts);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-  });
-}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
