30c85f4d1a1c570289eec59656d82f62cc201cce
David Han <davidhan.5.25@gmail.com>
2019-05-09 20:05:36 -0700

    Attempt four

--- server.js
+++ server.js
@@ -41,9 +41,11 @@ app.use('/api/users', users);
 app.use('/api/posts', posts);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.use(express.static('client/build'));
+  app.get('/*', (req, res) => {
+    let url = path.join(__dirname, '../client/build', 'index.html');
+    if (!url.startsWith('/app/')) url = url.substring(1);
+    res.sendFile(url);
   });
 }
 
