4512998641a5f084d7388eb268255f2a93a10ffe
David Han <davidhan.5.25@gmail.com>
2019-05-09 19:44:47 -0700

    Revert to last

--- server.js
+++ server.js
@@ -40,9 +40,11 @@ app.use('/api/profiles', profiles);
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('/client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
