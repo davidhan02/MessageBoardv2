b0b24ccbcee5b4ef642e247c393e3fe07e325d2f
David Han <davidhan.5.25@gmail.com>
2019-05-09 19:41:25 -0700

    Trying this out

--- server.js
+++ server.js
@@ -40,11 +40,9 @@ app.use('/api/profiles', profiles);
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
