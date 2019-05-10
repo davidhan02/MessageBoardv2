dd940372550768781e0021f746df8a4f2f8c7ff6
David Han <davidhan.5.25@gmail.com>
2019-05-09 20:26:21 -0700

    Last try

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ app.use('/api/users', users);
 app.use('/api/posts', posts);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
   });
 }
 
