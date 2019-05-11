ee318430c74bad8538ef4f05f839c2789f0b99bd
David Han <davidhan.5.25@gmail.com>
2019-05-10 22:16:50 -0700

    Possible fix to routing in heroku

--- server.js
+++ server.js
@@ -42,16 +42,9 @@ app.use('/api/posts', posts);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
-
-  app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join('client', 'build', 'index.html'));
   });
 }
 
