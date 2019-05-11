75bce00b5e3689b8667d00547a6da82d4b57f1fd
David Han <davidhan.5.25@gmail.com>
2019-05-10 22:20:08 -0700

    Attempt to fix heroku deployment route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
@@ -44,7 +45,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join('client', 'build', 'index.html'));
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
 
